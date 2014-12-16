f2ea94bfe21b373e168aad75b56d40e9f831fb0f
Yen-Hsun, Huang <s011208@gmail.com>
2014-12-16 14:15:29 +0800

    add preference categories

--- ProjectX/src/com/bj4/yhh/projectx/SettingPreference.java
+++ ProjectX/src/com/bj4/yhh/projectx/SettingPreference.java
@@ -1,6 +1,13 @@
 
 package com.bj4.yhh.projectx;
 
+import com.bj4.yhh.projectx.lot.Utils;
+import com.bj4.yhh.projectx.lot.blot.BLotParseService;
+import com.bj4.yhh.projectx.lot.hk6.HK6ParseService;
+import com.bj4.yhh.projectx.lot.lt539.LT539ParseService;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
@@ -28,18 +35,19 @@ public class SettingPreference extends PreferenceFragment {
         // grid color
         mGridColorPreference = (ListPreference)findPreference("grid_color");
         final int currentColor = mSharedPreferenceManager.getGridColor();
+
         switch (currentColor) {
             case SharedPreferenceManager.GRID_COLOR_BLUE:
-                mGridColorPreference.setDefaultValue("1");
+                mGridColorPreference.setValueIndex(1);
                 break;
             case SharedPreferenceManager.GRID_COLOR_BLACK:
-                mGridColorPreference.setDefaultValue("0");
+                mGridColorPreference.setValueIndex(0);
                 break;
             case SharedPreferenceManager.GRID_COLOR_GREY:
-                mGridColorPreference.setDefaultValue("2");
+                mGridColorPreference.setValueIndex(2);
                 break;
             default:
-                mGridColorPreference.setDefaultValue("1");
+                mGridColorPreference.setValueIndex(1);
                 break;
         }
         mGridColorPreference
@@ -65,8 +73,7 @@ public class SettingPreference extends PreferenceFragment {
 
         // long click delete
         mLongClickRemovePreference = (CheckBoxPreference)findPreference("allow_long_click_delete");
-        mLongClickRemovePreference.setDefaultValue(mSharedPreferenceManager
-                .isLongClickDeleteEnable());
+        mLongClickRemovePreference.setChecked(mSharedPreferenceManager.isLongClickDeleteEnable());
         mLongClickRemovePreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
 
             @Override
@@ -75,5 +82,19 @@ public class SettingPreference extends PreferenceFragment {
                 return true;
             }
         });
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if ("reset_all".equals(preference.getKey())) {
+            // reset all data and reload
+            final Activity activity = getActivity();
+            activity.stopService(new Intent(activity, HK6ParseService.class));
+            activity.stopService(new Intent(activity, LT539ParseService.class));
+            activity.stopService(new Intent(activity, BLotParseService.class));
+            // clear all db in advance
+            Utils.startUpdateAllService(activity);
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 }
